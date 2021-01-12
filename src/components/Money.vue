65d9ecf6c982642b8261ce4f08c06a1b5f237dba
EmmaLee <15830586201@163.com>
2021-01-12 17:47:55 +0800

    添加createAt和version控制，增加localStorage

--- src/components/Money.vue
+++ src/components/Money.vue
@@ -1,18 +1,16 @@
 <template>
     <Layout class-prefix="layout">
-        {{this.record}}}
-<!--        <number-pad :value="record.mount" @update:value="updateNumber"/>-->
-        <number-pad :value.sync="record.mount"/>
-        <note @update:value ="updateNote"/>
-        <tags :data-source.sync="tags" @update:value ="updateTags"/>
-        <types :value.sync="record.type" />
+        <number-pad :value.sync="record.mount" @submit="updateMount"/>
+        <note @update:value="updateNote"/>
+        <tags :data-source.sync="tags" @update:value="updateTags"/>
+        <types :value.sync="record.type"/>
     </Layout>
 
 </template>
 
 <script lang="ts">
     import Vue from "vue";
-    import {Component} from "vue-property-decorator";
+    import {Component, Watch} from "vue-property-decorator";
     import NumberPad from "@/components/Money/numberPad.vue";
     import Tags from "@/components/Money/tags.vue";
     import Types from "@/components/Money/types.vue";
@@ -23,29 +21,40 @@
         type: string;
         note: string;
         mount: number;
+        creatAt?: Date;
     }
+    window.localStorage.setItem('Version','0.1');
     @Component({
         components: {Note, Types, Tags, NumberPad}
     })
     export default class Money extends Vue {
         tags: string[] = ["衣", "食", "住", "行", "宠物", "蹦迪", "喝酒"];
-        record: Record={
-            tags:[],
-            type:'-',
-            note:'',
-            mount:0
+        record: Record = {
+            tags: [],
+            type: "-",
+            note: "",
+            mount: 0,
+
+        };
+        recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList')|| '[]');
+        updateMount() {
+            const oldRecord: Record = JSON.parse(JSON.stringify(this.record));
+            oldRecord.creatAt = new Date();
+            this.recordList.push(oldRecord);
+
         }
-        // updateType(value:string) {
-        //     this.record.type = value;
-        // }
-        // updateNumber(value:number){
-        //     this.record.mount = value;
-        // }
-        updateNote(value: string){
+
+        @Watch("recordList")
+        onRecordListChange() {
+            window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
+        }
+
+        updateNote(value: string) {
             this.record.note = value;
         }
-        updateTags(value: string[]){
-            this.record.tags = value
+
+        updateTags(value: string[]) {
+            this.record.tags = value;
         }
     }
 
--- src/components/Money/numberPad.vue
+++ src/components/Money/numberPad.vue
@@ -26,53 +26,60 @@
 <script lang="ts">
     import Vue from "vue";
     import {Component, Prop} from "vue-property-decorator";
+
     @Component
-    export default class NumberPad extends Vue{
-       @Prop() readonly value!: number;
-       output = this.value.toString();
-       inputContent(event: MouseEvent){
-           //强制执行类型
-           const button = event.target as HTMLButtonElement;
-           const input = button.textContent as string;
-           if(this.output.length>=16){
-               return;
-           }
-           if(this.output === '0'){
-               if(input ==='0'){
-                   return;
-               }else if('123456789'.indexOf(input)>=0){
-                   this.output = input;
-                   return this.output;
-               }else {
-                   this.output+=input;
-                   return this.output;
-               }
-           }else if(this.output.indexOf('.')>=0){
-               if(input==='.'){
-                   return ;
-               }else {
-                   this.output += input;
-                   return this.output;
-               }
-           }else {
-               return this.output+=input;
-           }
-       }
-       deleteNum(){
-           if(this.output.length === 1){
-               this.output = '0';
-               return this.output;
-           }else {
-               this.output = this.output.slice(0,-1);
-               return this.output;
-           }
-       }
-       removeAll(){
-            return this.output = '0';
-       }
-       ok(){
-            this.$emit("update:value",this.output)
-       }
+    export default class NumberPad extends Vue {
+        @Prop() readonly value!: number;
+        output = this.value.toString();
+
+        inputContent(event: MouseEvent) {
+            //强制执行类型
+            const button = event.target as HTMLButtonElement;
+            const input = button.textContent as string;
+            if (this.output.length >= 16) {
+                return;
+            }
+            if (this.output === "0") {
+                if (input === "0") {
+                    return;
+                } else if ("123456789".indexOf(input) >= 0) {
+                    this.output = input;
+                    return this.output;
+                } else {
+                    this.output += input;
+                    return this.output;
+                }
+            } else if (this.output.indexOf(".") >= 0) {
+                if (input === ".") {
+                    return;
+                } else {
+                    this.output += input;
+                    return this.output;
+                }
+            } else {
+                return this.output += input;
+            }
+        }
+
+        deleteNum() {
+            if (this.output.length === 1) {
+                this.output = "0";
+                return this.output;
+            } else {
+                this.output = this.output.slice(0, -1);
+                return this.output;
+            }
+        }
+
+        removeAll() {
+            return this.output = "0";
+        }
+
+        ok() {
+            this.$emit("update:value", this.output);
+            this.$emit("submit", this.output);
+            this.output = "0";
+        }
 
     }
 </script>
@@ -80,6 +87,7 @@
 <style lang="scss" scoped>
     .numberPad {
         $color-shadow: rgba(0, 0, 0, 0.25);
+
         .typePad {
             font-size: 36px;
             font-family: Consolas, monospace;
@@ -93,46 +101,57 @@
 
         .buttons {
             @extend %clearFix !optional;
+
             > button {
                 width: 25%;
                 height: 64px;
                 float: left;
                 background: transparent;
-                border:none;
+                border: none;
+
                 &.ok {
                     height: 64*2px;
                     float: right;
                 }
+
                 &.zero {
                     width: 2*25%;
                 }
-                $color:#E9E8D4;
-                &:nth-child(12){
+
+                $color: #E9E8D4;
+
+                &:nth-child(12) {
                     background: #3A465C;
                     color: $color;
                 }
-                &:nth-child(8),&:nth-child(11),&:nth-child(13){
-                    background: lighten(#3A465C,4%*3);
+
+                &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+                    background: lighten(#3A465C, 4%*3);
                     color: $color;
                 }
-                &:nth-child(14){
-                    background: lighten(#3A465C,4%*2);
+
+                &:nth-child(14) {
+                    background: lighten(#3A465C, 4%*2);
                     color: $color;
                 }
-                &:nth-child(4),&:nth-child(7),&:nth-child(10){
-                    background: lighten(#3A465C,4%*4);
+
+                &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+                    background: lighten(#3A465C, 4%*4);
                     color: $color;
                 }
-                &:nth-child(3),&:nth-child(6),&:nth-child(9){
-                    background: lighten(#3A465C,4%*5);
+
+                &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+                    background: lighten(#3A465C, 4%*5);
                     color: $color;
                 }
-                &:nth-child(2),&:nth-child(5){
-                    background: lighten(#3A465C,4%*6);
+
+                &:nth-child(2), &:nth-child(5) {
+                    background: lighten(#3A465C, 4%*6);
                     color: $color;
                 }
-                &:nth-child(1){
-                    background: lighten(#3A465C,4%*7);
+
+                &:nth-child(1) {
+                    background: lighten(#3A465C, 4%*7);
                     color: $color;
                 }
             }
