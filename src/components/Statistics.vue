bb8570be07b64571c9c34c03bd6174e568653534
EmmaLee <15830586201@163.com>
2021-02-17 22:10:00 +0800

    增加了列表的样式，限制了note显示的长度

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -3,11 +3,15 @@
             <Layout>
                 <Tabs :data-source="typeList" class-prefix="types" :value.sync="typesValue" />
                 <Tabs :data-source="dateList" class-prefix="interval" :value.sync="dateValue"/>
-                <ol>
-                    <li v-for="(items,index) in result" :key="index">
-                        <h3>{{items.title}}</h3>
-                        <ol>
-                            <li v-for="item in items.list" :key="item">{{item}}
+                <ol class="dateList">
+                    <li v-for="(items,index) in result" :key="index" >
+                            <h3 class="title">{{items.title}} <span class="sumMoney">{{'¥ '+items.sum}}</span></h3>
+
+                        <ol class="moneyList">
+                            <li v-for="item in items.list" :key="item.id" class="record">
+                                <span>{{tagString(item.tags)}}</span>
+                                <span class="note">{{limitShow(item.note)}}</span>
+                                <span>¥{{item.mount}}</span>
                             </li>
                         </ol>
                     </li>
@@ -29,16 +33,28 @@
         }
     )
     export default class Statistics extends Vue{
+        tagString(tags: Tag[]) {
+            return tags.length === 0 ? '无' : tags.join(',');
+        }
+        limitShow(note: string){
+            if(note.length>25){
+                const newNote = note.substring(0,14)+'...';
+                return newNote;
+            }else{
+                return note;
+            }
+        }
         get recordList(){
             return this.$store.state.recordList;
         }
         get result(){
             const recordList = this.recordList;
-            const hashTable: {[key: string]: {title: string;list: RecordItem[]}} ={};
+            const hashTable: {[key: string]: {title: string;list: RecordItem[];sum: number}} ={};
             for (let i=0;i<recordList.length;i++){
                 const date = recordList[i].creatAt.split('T')[0];
-                hashTable[date] = hashTable[date] || {title:date,list:[]};
-                hashTable[date].list.push(recordList[i].mount)
+                hashTable[date] = hashTable[date] || {title:date,list:[],sum:0};
+                hashTable[date].list.push(recordList[i]);
+                hashTable[date].sum += JSON.parse(recordList[i].mount);
             }
 
             return hashTable
@@ -69,7 +85,6 @@
         }
         .interval-tabs-item {
             padding: 10px;
-
             &.selected {
                 background: #3A465C;
                 color: #E9E8D4;
@@ -78,6 +93,33 @@
                     display: none;
                 }
             }
+        }
+        %item {
+            padding: 8px 16px;
+            line-height: 24px;
+            display: flex;
+            justify-content: space-between;
+            align-content: center;
+        }
+        .title {
+            @extend %item;
+            background: lighten(#495976,50%);
+            border-bottom: 1px solid #ccc;
+            font-weight: 500;
+            .sumMoney{
+                color: darken(#495976,20%);
+            }
+
+        }
+        .record {
+            @extend %item;
+            border-bottom: 1px solid #ccc;
+        }
+        .note {
+            margin-right: auto;
+            margin-left: 16px;
+            color: #999;
+
         }
 
     }
