c280705b847ec082332535a5d9e0c6fa78a091e9
EmmaLee <15830586201@163.com>
2021-02-19 01:13:15 +0800

    在屏幕显示大于400的时候添加二维码

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,20 @@
 <template>
     <Layout class-prefix="layout">
-        <number-pad :value.sync="record.mount" @submit="updateMount"/>
-        <note name='备注' placeholder="请在这里添加备注" :value="record.note" @update:value="updateNote"/>
-        <tags @update:value="updateTags"/>
-        <Tabs :value.sync="record.type" :data-source="typeList" class="title"/>
-
+<!--        <div class="qrcode" v-if="isShow">-->
+<!--            <img src="../../public/img/qrcode.png" alt="">-->
+<!--        </div>-->
+<!--        <div :class="{'forPop':screenWidth<=500&&screenWidth>=400,'remain':screenWidth< 400}" >-->
+            <number-pad :value.sync="record.mount" @submit="updateMount"/>
+            <note name='备注' placeholder="请在这里添加备注" :value="record.note" @update:value="updateNote"/>
+            <tags @update:value="updateTags"/>
+            <Tabs :value.sync="record.type" :data-source="typeList" class="title"/>
+<!--        </div>-->
+        <div class="box" v-if="isShow" @click="showOff">
+               <div class="container">
+                   <img src="../../public/img/qrcode.png" alt="">
+                   <h3>请扫二维码，使用手机观看效果更加！：）</h3>
+             </div>
+             </div>
     </Layout>
 
 </template>
@@ -20,12 +30,32 @@
 
 
     window.localStorage.setItem("Version", "0.1");
+    window.localStorage.setItem('count','0');
     @Component({
         components: {Tabs, Note, Tags, NumberPad}
     })
     export default class Money extends Vue {
+        isShow = false;
+
+
+
+
+        showCode(isShow: boolean){
+            if (window.innerWidth>450){
+                console.log(window.innerWidth);
+                this.isShow = true;
+                console.log(this.isShow);
+                // window.alert('请扫二维码，使用手机观看效果更加！：）');
+                return this.isShow;
+            }
+        }
         get recordList() {
             return this.$store.state.recordList;
+        }
+        showOff(){
+            this.isShow = false;
+            window.localStorage.setItem('count','1');
+
         }
 
         typeList = typeList;
@@ -36,10 +66,13 @@
             mount: 0,
 
         };
-
         //recordList = this.$store.state.recordList;
         created() {
             this.$store.commit("fetchRecord");
+            if(window.localStorage.getItem('count')==='0'){
+                this.showCode(this.isShow);
+            }
+
         }
 
         updateMount() {
@@ -58,19 +91,43 @@
         updateTags(value: string[]) {
             this.record.tags = value;
         }
+
     }
 
 </script>
 <style lang="scss">
     .layout-content {
-        /*border: 1px solid red;*/
         display: flex;
         flex-direction: column-reverse;
+        .box {
+            /*display: none;*/
+            width: 100%;
+            height: 100%;
+            position: fixed;
+            top: 0;
+            left: 0;
+            background: rgba(51,51,51,0.5);
+            z-index: 3;//根据自己页面情况设置
+        }
+        .container img {
+            position: absolute;
+            top: 50%;//以下3行设置弹框居中页面，根据自己页面情况选择
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background: #fff;
+            z-index: 5;//根据自己页面情况设置
+        }
+        .container h3 {
+            position: absolute;
+            top: 75%;//以下3行设置弹框居中页面，根据自己页面情况选择
+            left: 50%;
+            transform: translate(-50%, -50%);
 
-        > .title {
-
+            z-index: 5;
+            color: white;
         }
     }
+
 </style>
 
 <style lang="scss" scoped>
