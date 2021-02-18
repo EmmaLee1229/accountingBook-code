84027931da8a6e59526bacc7d533ef520eccfc0c
EmmaLee <15830586201@163.com>
2021-02-18 22:21:06 +0800

    修改git地址

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:git@github.com:EmmaLee1229/accountingBook.git master:gh-pages
+git push -f git@github.com:EmmaLee1229/accountingBook.git master:gh-pages
 
 cd -
